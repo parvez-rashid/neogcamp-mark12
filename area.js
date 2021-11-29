29cb818255d572864bdd13170766397a22985a8b
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2021-11-29 23:06:50 +0530

    Fix -ve and 0 value, update style

--- area.js
+++ area.js
@@ -6,7 +6,7 @@ function calculateTriangleArea() {
     const base = sides[0].value;
     const height = sides[1].value;
 
-    const triangleArea = 0.5 * Number(base) * Number(height);
+    const triangleArea = (0.5 * Number(base) * Number(height)).toFixed(2);
     outputArea.innerText = "The area of the triangle is " + triangleArea + " cm2";
 }
 
